6a6c4fc0f4b3c9fb4002016060728e2ce46a0426
Filipp Filippov <f.filippov7@gmail.com>
2018-07-28 19:57:18 +0300

    reworked env setup

--- goinit.sh
+++ goinit.sh
@@ -3,14 +3,16 @@
 #Some of this really not obvious for beginner
 #We gonna use verion that hardcoded here. Probably one day i'll rework it.
 
-cd /tmp\
-&& curl 'https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz' --output 'go.tar.gz'
-tar -xvf 'go.tar.gz' -C /opt 
+curl 'https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz' --output /tmp/'go.tar.gz'
+tar -xvf /tmp/'go.tar.gz' -C /opt 
+
 ###Workdir
 mkdir -p /opt/forge/src
 
-#Export env
-export GOROOT=/opt/go
-export GOPATH=$GOROOT
-export GOBPATH=/opt/forge
-export PATH=$PATH:$GOROOT/bin
+##Copy env file, update bashrc
+SRCDIR="$(dirname "$0")"
+cp $SRCDIR/goenv $HOME/\.goenv
+if [ $(grep -q 'goenv' $HOME/\.bashrc; echo $?) = "1" ];
+then
+ echo "source $HOME/.goenv" >> $HOME/\.bashrc
+fi
